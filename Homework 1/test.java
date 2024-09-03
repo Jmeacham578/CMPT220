3142c454b916f5f5b387ce5d460eb8dd9b9ea67b
Jmeacham578 <jack.meacham1@marist.edu>
2024-09-03 13:25:03 -0400

    class

---
+++ Homework 1/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        String name = "Jack";
+        Integer age = 20;
+        System.out.println("My name is " + name+ " and I am " + age);
+        
+
+    }
+}
